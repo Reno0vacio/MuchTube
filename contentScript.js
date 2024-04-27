c88399800eb31c73ae252530f447ea7a116e5527
Reno0vacio <horkrux78@gmail.com>
2024-04-27 20:06:06 +0200

    HTML element's aded, styles aded

--- contentScript.js
+++ contentScript.js
@@ -1,33 +1,34 @@
-// Ez a script töltődik be az oldalba ahol vagyunk.
-(() => {
-    console.log("hello");
-    function asd() {
-        let yTContent = document.getElementById("full-bleed-container");
-        let mainVideo = document.getElementsByClassName("ytp-overlay ytp-speedmaster-overlay")[0];
-        let mainVideoContorls = document.getElementsByClassName("ytp-gradient-bottom")[0];
-        mainVideo.setAttribute("style", "display: flex; justify-content: center; align-items: center; background-color: rgba(0, 0, 0, 0.9);");
-        let question = document.createElement("p");
-        let br = document.createElement("br");
-        let input = document.createElement("input");
+function timeOut(second) {
+    setTimeout(() => {
+        const mainVideo = document.getElementsByClassName("style-scope ytd-player")[0].parentElement;
+        const videoSize = document.getElementsByClassName("video-stream html5-main-video")[0].getAttribute("style").split(";");
+        const videoWidth = videoSize[0];
+        const videoHeight = videoSize[1];
 
-        question.appendChild(document.createTextNode("Miért nézed ezt a videót?: "));
+        const div = document.createElement("div");
+        const question = document.createElement("h1");
+        const questionText = document.createTextNode("Why are you watching this video?");
+        const input = document.createElement("textarea");
+        const br = document.createElement("br");
 
-        input.setAttribute("placeholder", "Mert...")
-        question.setAttribute("style", "font-size: 30px;")
+        input.setAttribute("placeholder", "because of...");
+        input.setAttribute("style", "width: 250px; height: 27px; resize: none; border: 1px solid black; border-radius:12px; background-color: rgba(0, 0, 0, 0.7); color: #f3f3f3e0");
+        question.setAttribute("style", "color: #f3f3f3e0; font-size: 3rem; margin-bottom: 5px;")
+        div.setAttribute("style", "display: flex; justify-content: center; flex-direction: column; align-items: center; position: absolute; background: rgba(0,0,0, 0.7); backdrop-filter: blur(10px); border-radius: 12px; " + videoHeight + "; " + videoWidth + "; " + "top: 0px;")
+        question.appendChild(questionText);
+        div.append(question, br, input);
+        mainVideo.append(div)
+        input.addEventListener("keydown", (event) => {
+            if (event.key === "Enter" && input.value != "") {
+                div.remove();
+            }
+        })
+    }, second);
+}
+timeOut(2000);
 
-        mainVideo.appendChild(question);
-        mainVideo.appendChild(br);
-        mainVideo.appendChild(input);
 
-        mainVideoContorls.setAttribute("stlye", "background: rgba(23, 23, 23, 0.044)");
-        console.log("Sikerült");
-
-    }
-    setTimeout(asd, 1500);
-})();
-
-
-
-// Mindig az "utolsó" inline css stilus lesz aktuális.
-// Meg kell várni hogy betöltődjenek az elmek.
+//What i learned: 
+// css styles: backdrop-filter, .
+// timeout.
 
